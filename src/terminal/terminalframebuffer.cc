2cce24cb05615e50b5c675b0693ea9c89f144b6b
Stanislav Spassov <stanspas@amazon.de>
2019-08-03 19:32:57 +0200

    Add support for ECMA-48 escape sequence for faint/half-bright
    ECMA-48 defines rendition mode 2 as:
      faint, decreased intensity or second colour
    
    Faint mode shares its inverse (22) with bold (1):
      normal colour or normal intensity (neither bold nor faint)
    
    The Renditions::attribute_type enum already included 'faint'
    but the value remained unused until now.
    
    Signed-off-by: Stanislav Spassov <stanspas@amazon.de>

--- src/terminal/terminalframebuffer.cc
+++ src/terminal/terminalframebuffer.cc
@@ -495,8 +495,14 @@ void Renditions::set_rendition( color_type num )
   bool value = num < 9;
   switch ( num ) {
     case 1:
+      set_attribute( bold, value );
+      break;
+    case 2:
+      set_attribute( faint, value );
+      break;
     case 22:
       set_attribute( bold, value );
+      set_attribute( faint, value );
       break;
     case 3:
     case 23:
@@ -574,6 +580,8 @@ std::string Renditions::sgr( void ) const
   ret.append( "\033[0" );
   if ( get_attribute( bold ) )
     ret.append( ";1" );
+  if ( get_attribute( faint ) )
+    ret.append( ";2") ;
   if ( get_attribute( italic ) )
     ret.append( ";3" );
   if ( get_attribute( underlined ) )
