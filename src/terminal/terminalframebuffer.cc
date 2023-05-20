e4ed6560f18534ffd2e4f2e7fc430d74e0b9e637
John Drouhard <john@drouhard.dev>
2023-05-19 20:12:37 -0500

    undercurl/underline color support

--- src/terminal/terminalframebuffer.cc
+++ src/terminal/terminalframebuffer.cc
@@ -455,7 +455,7 @@ void DrawState::resize( int s_width, int s_height )
 }
 
 Renditions::Renditions( color_type s_background )
-  : foreground_color( 0 ), background_color( s_background ), attributes( 0 )
+  : foreground_color( 0 ), background_color( s_background ), underline_color( 0 ), attributes( 0 )
 {}
 
 /* This routine cannot be used to set a color beyond the 16-color set. */
@@ -463,7 +463,7 @@ void Renditions::set_rendition( color_type num )
 {
   if ( num == 0 ) {
     clear_attributes();
-    foreground_color = background_color = 0;
+    foreground_color = background_color = underline_color = 0;
     return;
   }
 
@@ -472,6 +472,9 @@ void Renditions::set_rendition( color_type num )
     return;
   } else if ( num == 49 ) {
     background_color = 0;
+    return;
+  } else if ( num == 59 ) {
+    underline_color = 0;
     return;
   }
 
@@ -500,8 +503,16 @@ void Renditions::set_rendition( color_type num )
       set_attribute( italic, value );
       break;
     case 4:
+    case 401:
+      set_attribute( underlined, true );
+      break;
     case 24:
-      set_attribute( underlined, value );
+    case 400:
+      set_attribute( underlined, false );
+      set_attribute( underline_double, false );
+      set_attribute( underline_curl, false );
+      set_attribute( underline_dotted, false );
+      set_attribute( underline_dashed, false );
       break;
     case 5:
     case 25:
@@ -514,6 +525,18 @@ void Renditions::set_rendition( color_type num )
     case 8:
     case 28:
       set_attribute( invisible, value );
+      break;
+    case 402:
+      set_attribute( underline_double, true );
+      break;
+    case 403:
+      set_attribute( underline_curl, true );
+      break;
+    case 404:
+      set_attribute( underline_dotted, true );
+      break;
+    case 405:
+      set_attribute( underline_dashed, true );
       break;
     default:
       break; /* ignore unknown rendition */
@@ -538,6 +561,11 @@ void Renditions::set_background_color( int num )
   }
 }
 
+void Renditions::set_underline_color( int num )
+{
+  underline_color = num;
+}
+
 std::string Renditions::sgr( void ) const
 {
   std::string ret;
@@ -556,6 +584,14 @@ std::string Renditions::sgr( void ) const
     ret.append( ";7" );
   if ( get_attribute( invisible ) )
     ret.append( ";8" );
+  if ( get_attribute( underline_double ) )
+    ret.append( ";4:2" );
+  if ( get_attribute( underline_curl ) )
+    ret.append( ";4:3" );
+  if ( get_attribute( underline_dotted ) )
+    ret.append( ";4:4" );
+  if ( get_attribute( underline_dashed ) )
+    ret.append( ";4:5" );
 
   if ( foreground_color ) {
     // Since foreground_color is a 25-bit field, it is promoted to an int when
@@ -596,6 +632,19 @@ std::string Renditions::sgr( void ) const
       // See comment above about explicit promotion; it applies here as well.
       int bg = background_color;
       snprintf( col, sizeof( col ), ";%d", bg );
+    }
+    ret.append( col );
+  }
+  if ( underline_color ) {
+    if ( is_true_color( underline_color ) ) {
+      snprintf( col,
+                sizeof( col ),
+                ";58:2::%d:%d:%d",
+                ( underline_color >> 16 ) & 0xff,
+                ( underline_color >> 8 ) & 0xff,
+                underline_color & 0xff );
+    } else {
+      snprintf( col, sizeof( col ), ";58:5:%d", underline_color );
     }
     ret.append( col );
   }
